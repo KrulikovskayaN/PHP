56601e5122a1428dfb5aa1b2b25ca54959d06e36
krulikovskaya <krulikovskaya@rozetka.com.ua>
2021-03-05 00:42:54 +0200

    formatting

--- 5/oldcode_lesson_18.php
+++ 5/oldcode_lesson_18.php
@@ -9,20 +9,20 @@
 echo 'Упражнение 1';
 echo '<br>';
 
-function translit(string $str){
-		$ru = [ 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й',
-	       	'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
-	       	'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
-	       	'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
-	       	'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
-	       	'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' ];
-		$en = [ 'A', 'B', 'V', 'G', 'D', 'E', 'IO', 'ZH', 'Z', 'I', 'I',
-	       	'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F',
-	       	'H', 'C', 'CH', 'SH', 'SH', '`', 'Y', '`', 'E', 'IU', 'IA',
-	       	'a', 'b', 'v', 'g', 'd', 'e', 'io', 'zh', 'z', 'i', 'i',
-	       	'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f',
-	       	'h', 'c', 'ch', 'sh', 'sh', '`', 'y', '`', 'e', 'iu', 'ia' ];
-		return str_replace($ru, $en, $str);
+function translit(string $str): string {
+    $ru = [ 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й',
+            'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
+            'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
+            'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+            'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я' ];
+    $en = [ 'A', 'B', 'V', 'G', 'D', 'E', 'IO', 'ZH', 'Z', 'I', 'I',
+            'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F',
+            'H', 'C', 'CH', 'SH', 'SH', '`', 'Y', '`', 'E', 'IU', 'IA',
+            'a', 'b', 'v', 'g', 'd', 'e', 'io', 'zh', 'z', 'i', 'i',
+            'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f',
+            'h', 'c', 'ch', 'sh', 'sh', '`', 'y', '`', 'e', 'iu', 'ia' ];
+    return str_replace($ru, $en, $str);
 }
 echo translit('Сделайте функцию, которая принимает строку на русском языке, а возвращает ее транслит');
 echo '<br>';
@@ -40,81 +40,94 @@ echo '<br>';
 
 /* 1 вариант
 
-function form($num, $form1, $form2, $form3) {
-	$result = $num . ' ' . $form3;
-	if (1 === $num % 10 && 11 != $num % 100) {
-		$result = $num . ' ' . $form1;
-    	} elseif (($num % 10 > 1 && $num % 10 < 5) && ($num % 100 > 15 || $num % 100 < 11)) {
-    		$result = $num . ' ' . $form2;
-    	}
-    	
-    	return $result;
+function form(int $num, string $form1, string $form2, string $form3): string {
+    
+    $result = $num . ' ' . $form3;
+    
+    if (1 === $num % 10 && 11 != $num % 100) {
+        $result = $num . ' ' . $form1;
+    } elseif (($num % 10 > 1 && $num % 10 < 5) && ($num % 100 > 15 || $num % 100 < 11)) {
+        $result = $num . ' ' . $form2;
+    }
+    
+    return $result;
 }
 echo form(4,'яблоко', 'яблока', 'яблок' );
 */
 
 /* 2 вариант
 
-function form($num, $form1, $form2, $form3) {
-	$num1 = $num % 10; // получаем последнюю цифру
-	$num2 = $num % 100; // получаем последние две цифры
-	
-	$result = $num . ' ' . $form3;
-	if (1 === $num1 && 11 != $num1) {
-		$result = $num . ' ' . $form1;
-	} elseif (($num1 > 1 && $num1 < 5) && ($num2 > 15 || $num2 < 11)) {
-		$result = $num . ' ' . $form2;
-	}
+function form(int $num, string $form1, string $form2, string $form3): string {
 
-    	return $result;
+    $num1 = $num % 10; // получаем последнюю цифру
+    $num2 = $num % 100; // получаем последние две цифры
+	
+    $result = $num . ' ' . $form3;
+    
+    if (1 === $num1 && 11 != $num1) {
+       $result = $num . ' ' . $form1;
+    } elseif (($num1 > 1 && $num1 < 5) && ($num2 > 15 || $num2 < 11)) {
+       $result = $num . ' ' . $form2;
+    }
+    
+    return $result;
 }
 echo form(4,'яблоко', 'яблока', 'яблок' );
 */
 
+
 // 3 вариант
 	
-function form(string $num, $form1, $form2, $form3) {
+function form(int $num, string $form1, string $form2, string $form3): string {
+    
     $num1 = substr($num, -2); // получаем последние две цифры
     $num2 = substr($num, -1); // получаем последнюю цифру
     
-switch($num2) {
-	case '1': 
-		$result = $num . ' ' . $form1; 
-	break;
-	case '2':
-	case '3':
-	case '4':
-		$result = $num . ' ' . $form2; 
-        break;
+    switch ($num2) {
+        case '1':
+            $result = $num . ' ' . $form1; 
+	    break;
+        case '2':
+        case '3':
+        case '4':
+            $result = $num . ' ' . $form2; 
+            break;
         default:
-        	$result = $num . ' ' . $form3; 
-        break;
-     }
-if ($num1 >10 && $num1 < 20) {
-	$result = $num . ' ' . $form3;
-	}    
-     return $result;
-
+            $result = $num . ' ' . $form3; 
+            break;
+    }
+        
+    if ($num1 >10 && $num1 < 20) {
+        $result = $num . ' ' . $form3;
+    }    
+     
+    return $result;
 }
 echo form('22', 'яблоко', 'яблока', 'яблок');	
 echo '<br>';
+
+
 
 // 3.Найдите все счастливые билеты. Счастливый билет - это билет, в котором сумма первых трех цифр
 // его номера равна сумме вторых трех цифр его номера.
 echo 'Упражнение 3';
 echo '<br>';
 
-function happyTicket(string $number) {
-		$left_num = substr($number,0,3);
-		$right_num = substr($number,strlen($left_num)); 
-			$right_sum = array_sum(str_split($right_num));
-			$left_sum = array_sum(str_split($left_num)); 
-				if ($right_sum === $left_sum) {
-					$ticket = 'Happy ticket';
-				} else {
-					$ticket =  'Try one more time';
-				}		
-				return $ticket;
+function happyTicket(string $number): string {
+
+    $left_num = substr($number,0,3);
+    $right_num = substr($number,strlen($left_num));
+    
+    $right_sum = array_sum(str_split($right_num));
+    $left_sum = array_sum(str_split($left_num));
+        
+    if ($right_sum === $left_sum) {
+        $ticket = 'Happy ticket';
+    } else {
+        $ticket = 'Try one more time';
+    }
+    
+    return $ticket;
 }									
 echo happyTicket('937992');
 echo '<br>';
@@ -132,39 +145,44 @@ echo '<br>';
 echo 'Упражнение 4';
 echo '<br>';
 
-for($i=1; $i<=10000; $i++) {
-	for($j=1; $j<=$i; $j++) {
-		friendlyNumbers($i,$j);
-	}
+for ($i=1; $i<=10000; $i++) {
+    for ($j=1; $j<=$i; $j++) {
+        friendlyNumbers($i,$j);
+    }
 }		
 
 //функция находит все делители заданного числа и возвращает их в виде массива
 
-function getDivisors(int $num) { 
-	$result = [];
-	for ($i=1; $i < $num; $i++) {
-		if ($num % $i === 0) {
-			$result[] = $i;
-		}
-	}
-	return $result;	
-	}
+function getDivisors(int $num): array { 
+    
+    $result = [];
+	
+    for ($i=1; $i < $num; $i++) {
+        if ($num % $i === 0) {
+            $result[] = $i;
+        }
+    }
+    
+    return $result;	
+}
 //print_r(getDivisors(220));
 
 //функция параметром принимает массив и возвращает его сумму
 
-function getDigitsSum(array $result) {
-	$sum = 0;
-	foreach($result as $res) {
-		$sum += $res;
-	}
-	return $sum;
+function getDigitsSum(array $result): int {
+	
+    $sum = 0;
+	
+    foreach($result as $res) {
+        $sum += $res;
+    }
+	
+    return $sum;
 }
 //echo getDigitsSum($result = array(1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110));
 
-function friendlyNumbers($i,$j) {
-	if (getDigitsSum(getDivisors($i)) === $j && getDigitsSum(getDivisors($j))===$i && $i !== $j) {
-		echo $i . ' and ' . $j . '<br>';
-	
-	}
+function friendlyNumbers(int $i, int $j) {
+    if (getDigitsSum(getDivisors($i)) === $j && getDigitsSum(getDivisors($j))===$i && $i !== $j) {
+        echo $i . ' and ' . $j . '<br>';
+    }
 }
--- 5/oldcode_lesson_19.php
+++ 5/oldcode_lesson_19.php
@@ -9,9 +9,10 @@
 // по умолчанию принимать значение 10.
 
 function cut(string $string, int $a=10) {
-	$string = mb_substr($string, 0, $a,'UTF-8');
-	return $string;
-	}
+    $string = mb_substr($string, 0, $a,'UTF-8');
+    
+    return $string;
+}
 var_dump (cut('Мечтать не вредно, вредно не мечтать', 20));
 echo '<br>';
 var_dump (cut('If length is given and is 0, false or null, an empty string will be returned.', 20));
@@ -22,11 +23,12 @@ echo '<br>';
 $arr = [1, 2, 3, 4, 5, 6, 7];
 
 function all(array $arr) {
-	echo array_shift($arr) . '<br>'; // первый элемент массива
+    
+    echo array_shift($arr) . '<br>'; // первый элемент массива
 	
-	if(!empty($arr)) {
-		all($arr);
-	}
+    if(!empty($arr)) {
+        all($arr);
+    }
 }	
 	
 all($arr);
@@ -36,12 +38,14 @@ echo '<br>';
 // пока сумма не станет однозначным числом (9 и менее).
 
 function lowNum(int $num) {
-	$result = array_sum(str_split($num, 1));
+    
+    $result = array_sum(str_split($num, 1));
 	
-	if ($result > 9) {
-		$result = lowNum($result);
-	}
-	return $result;
+    if ($result > 9) {
+        $result = lowNum($result);
+    }
+    
+    return $result;
 }
 
 var_dump (lowNum(12345678912));
