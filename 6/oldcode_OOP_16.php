ec30b43aae53794ff904ceb8ec0f697b6fc67f56
krulikovskaya <krulikovskaya@rozetka.com.ua>
2021-06-14 00:26:02 +0300

    lesson 16

---
+++ 6/oldcode_OOP_16.php
@@ -0,0 +1,23 @@
+<?php
+
+// 16.1. Реализуйте класс Arr и вызовите его метод getSum сразу после создания объекта.
+
+class Arr {
+    public array $numbers = [];
+    
+    public function __construct($numbers) {
+        $this->numbers = $numbers;
+    }
+    
+    public function add($number): void {
+        $this->numbers[] = $number;    
+    }
+    
+    public function getSum(): int {
+        return array_sum($this->numbers);
+    }
+}
+
+echo (new Arr([10, 20, 30]))->getSum();
+
+            
