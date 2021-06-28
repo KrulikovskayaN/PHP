949c4b0984a403763bb6cbe81fe0483083c50da1
kate <balan.k@rozetka.com.ua>
2021-06-28 23:23:16 +0300

    check 18

--- 6/oldcode_OOP_18.php
+++ 6/oldcode_OOP_18.php
@@ -4,50 +4,49 @@
 
 class ArrayAvgHelper {
 
-// Находит сумму первых степеней элементов массива
-
-    public function getAvg1(array $arr): int {
+    // Находит сумму первых степеней элементов массива
+    // Краще тут float, оскільки з задачі немає обмежень стосовно того,
+    // що числа в масиві обов'язково будуть цілими
+    public function getAvg1(array $arr): float {
         return $this->getSum($arr, 1);    
     }
-    
-// Находит сумму вторых степеней элементов массива и извлекает из нее квадратный корень
 
+    // Находит сумму вторых степеней элементов массива и извлекает из нее квадратный корень
+    // Покращення: коментарі до функцій пишуться на тому ж рівні на якому ця функція
+    // тобто якщо перед функцією один таб, то і перед коментарем один таб буде
     public function getAvg2(array $arr): float {
         $result = $this->getSum($arr, 2);
         return $this->calcSqrt($result, 1/2); 
     }
-    
-// Находит сумму третьих степеней элементов массива и извлекает из нее кубический корень
 
+    // Находит сумму третьих степеней элементов массива и извлекает из нее кубический корень
+    // Функція getSum() повертає float, а перший параметр calcSqrt() повинен бути int
     public function getAvg3(array $arr): float {
         $result = $this->getSum($arr, 3);
         return $this->calcSqrt($result, 1/3);    
     }
-    
-// Находит сумму четвертых степеней элементов массива и извлекает из нее корень четвертой степени
 
+    // Находит сумму четвертых степеней элементов массива и извлекает из нее корень четвертой степени
     public function getAvg4(array $arr): float {
        $result = $this->getSum($arr, 4);
        return $this->calcSqrt($result, 1/4);   
     
     }
-    
-// Вспомогательный метод, который параметром принимает массив и степень и возвращает сумму степеней
-// элементов массива
 
+    // Вспомогательный метод, который параметром принимает массив и степень и возвращает сумму степеней
+    // элементов массива
     private function getSum(array $arr, float $power): float {
         $result = 0;
         
         foreach ($arr as $elem) {
             $result += pow($elem, $power);    
+        }
+        return $result;
     }
-    return $result;
-}        
-    
-// Впомогательный метод, который параметром принимает целое число и степень и возвращает корень
-// заданной степени из числа
 
-    private function calcSqrt(int $num, float $power): float {
+    // Впомогательный метод, который параметром принимает целое число и степень и возвращает корень
+    // заданной степени из числа
+    private function calcSqrt(float $num, float $power): float {
         return pow($num, $power);
     }
 }
@@ -55,11 +54,11 @@ class ArrayAvgHelper {
 
 $arrayAvgHelper = new ArrayAvgHelper;
 
+// краще перевіряти і з десятковими числами теж
+// $arr = [11.3, 5.7, 9.3];
 $arr = [1, 5, 9];
 
 echo $arrayAvgHelper->getAvg1($arr) . '<br>';
 echo $arrayAvgHelper->getAvg2($arr) . '<br>';
 echo $arrayAvgHelper->getAvg3($arr) . '<br>';
 echo $arrayAvgHelper->getAvg4($arr) . '<br>';
-
-                    
